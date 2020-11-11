3cc901fccd62c0615e3760582a69964cb6517f11
Tanishqa <tanishqamahendru@gmail.com>
2020-11-11 14:56:49 +0530

    11-11-20 02:55pm site is redirecting to their respective end points acc to prediction

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 from ML import livePredictions
 
 app = Flask(__name__)
@@ -10,14 +10,39 @@ def home():
         with open('audio.wav', 'wb') as audio:
             f.save(audio)
         print('file uploaded successfully')
+        return redirect("/pred")
 
-        pred = livePredictions(path='SER_model.h5',file='audio.wav')
-        pred.load_model()
-        prediction = pred.makepredictions()
-        print(prediction)
-        return render_template('index.html', prediction=prediction)
+        # pred = livePredictions(path='SER_model.h5',file='audio.wav')
+        # pred.load_model()
+        # prediction = pred.makepredictions()
+        # print(prediction)
+        # if(prediction=='calm'):
+        # 	return redirect("/disgust")
+        # if(prediction=='disgust'):
+        # 	return redirect("/disgust")
+        # print("abc")
     
     return render_template('index.html')
+
+@app.route('/pred', methods=["GET","POST"])
+def pred():
+    pred = livePredictions(path='SER_model.h5',file='audio.wav')
+    pred.load_model()
+    prediction = pred.makepredictions()
+    print(prediction)
+    if(prediction=='calm'):
+        return redirect("/calm")
+    if(prediction=='disgust'):
+        return redirect("/disgust")
+    return "Hello"
+
+@app.route('/disgust', methods=["GET","POST"])
+def disgust():    
+    return render_template('disgust.html')
+
+@app.route('/calm', methods=["GET","POST"])
+def calm():    
+    return render_template('calm.html')
 
 if __name__ == "__main__":
 	app.run(debug=True)
